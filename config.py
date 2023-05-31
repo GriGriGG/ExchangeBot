4383ef2a76b2cf994ba389629de2bb365613088c
Grigorii Riabokon <le1taon1@gmail.com>
2023-05-31 13:33:07 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import telebot
+
+TOKEN = "5870015273:AAEu2WOr0rgioq0f8k6-JfKyytYw7hqbieQ"
+
+bot = telebot.TeleBot(TOKEN)
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
